be957abcca05efb62dab7565ac242fee89ae8a14
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-09-22 11:26:49 +0100

    Clicky effects into a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import React from "react";
+
+function shuffle(as) {
+  let ci = as.length;
+
+  while (ci !== 0) {
+    const i = Math.floor(Math.random() * ci);
+    ci--;
+
+    const tmp = as[ci];
+    as[ci] = as[i];
+    as[i] = tmp;
+  }
+
+  return as;
+}
+
+function Nums(props) {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6]);
+
+  useEffect(() => {
+    setNumbers([...shuffle(numbers)]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.trigger]);
+
+  return (
+    <div className="numsSetContainer">
+      <h3>{props.id}</h3>
+      <div className="numsSet">
+        {numbers.map((num) => (
+          <div className="num" key={num}>
+            {num}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const numNums = Array(10).fill(0);
+  const [count, setCount] = useState(0);
+
+  function inc() {
+    setCount((n) => n + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Effects!</h1>
+      <button onClick={inc}>Shuffle all numbers!</button>
+      <div className="nums">
+        {numNums.map((e, i) => (
+          <Nums key={i} id={i + 1} trigger={count}></Nums>
+        ))}
+        <Nums id={"Fixed"}></Nums>
+      </div>
     </div>
   );
 }
