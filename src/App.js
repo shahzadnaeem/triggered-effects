da9422f97a00986c9af282be9598f0798dc600d7
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-09-23 10:46:00 +0100

    Sync with demo and add tests, cleanup

--- src/App.js
+++ src/App.js
@@ -20,31 +20,42 @@ function shuffle(as) {
 function Nums(props) {
   const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6]);
 
+  function isFixed() {
+    return props.id === "Fixed"
+  }
   useEffect(() => {
-    setNumbers([...shuffle(numbers)]);
+    if (!isFixed()) {
+      setNumbers([...shuffle(numbers)]);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.trigger]);
 
   return (
     <div className="numsSetContainer">
-      <h3>{props.id}</h3>
+      <h3>Box - {props.id}</h3>
       <div className="numsSet">
-        {numbers.map((num) => (
-          <div
-            className={`num ${(props.trigger & 1) === 1 ? "alt" : ""}`}
-            key={num}
-          >
-            {num}
-          </div>
-        ))}
+        {numbers.map((num) => {
+          let classes = "num";
+          if (!isFixed() && (props.trigger & 1)) {
+            classes += " alt"
+          }
+
+          return (
+            <div className={classes} key={num}>
+              {num}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 function App() {
-  const numNums = Array(10).fill(0);
+  const numNums = Array(20).fill(0);
   const [count, setCount] = useState(0);
+
+  numNums.splice(10, 0, "Fixed");
 
   function inc() {
     setCount((n) => n + 1);
@@ -56,11 +67,7 @@ function App() {
       <button onClick={inc}>Shuffle all numbers!</button>
       <div className="nums">
         {numNums.map((e, i) => (
-          <Nums key={i} id={i + 1} trigger={count}></Nums>
-        ))}
-        <Nums id={"Fixed"}></Nums>
-        {numNums.map((e, i) => (
-          <Nums key={i} id={i + 11} trigger={count}></Nums>
+          <Nums key={i} id={e !== "Fixed" ? i + 1 : e} trigger={count}></Nums>
         ))}
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1 with Effects!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1El = screen.getByText(/Effects!/i);
+  expect(h1El).toBeInTheDocument();
+});
+
+test('renders 20 numbered Boxes', () => {
+  render(<App />);
+  const numsBoxes = screen.getAllByText(/Box - [0-9]+/);
+  expect(numsBoxes.length).toBe(20)
+});
+
+test('renders 1 Fixed box', () => {
+  render(<App />);
+  const numsBoxes = screen.getAllByText(/Box - Fixed/);
+  expect(numsBoxes.length).toBe(1)
 });
